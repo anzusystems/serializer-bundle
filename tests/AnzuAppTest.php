ed022ff812cb278768957bd1fcbe847a7570e5a2
sakulb <lukas.budos@gmail.com>
2023-11-09 10:16:29 +0100

    Ref AB#73574 WIP Add test.

--- tests/AnzuAppTest.php
+++ tests/AnzuAppTest.php
@@ -12,16 +12,40 @@ final class AnzuAppTest extends AbstractTestCase
 {
     /**
      * @throws SerializerException
+     * @dataProvider data
      */
-    public function testTes(): void
+    public function testSerializeBasic(string $json, Example $data): void
     {
-        $deserialized = (new Example())
-            ->setId(1)
-            ->setName('Test name')
-            ->setCreatedAt(new DateTimeImmutable('2023-12-31T00:00:00Z'))
-        ;
-        $serialized = '{"id":1,"name":"Test name","createdAt":"2023-12-31T00:00:00Z"}';
-        $actual = $this->serializer->serialize($deserialized);
-        self::assertEquals($serialized, $actual);
+        $serialized = $this->serializer->serialize($data);
+        self::assertEquals($json, $serialized);
+    }
+
+    /**
+     * @throws SerializerException
+     * @dataProvider data
+     */
+    public function testDeSerializeBasic(string $json, Example $data): void
+    {
+        $deserialized = $this->serializer->deserialize($json, $data::class);
+        self::assertEquals($data, $deserialized);
+    }
+
+    public function data(): iterable
+    {
+        yield [
+            '{"id":1,"name":"Test name","createdAt":"2023-12-31T12:34:56Z"}',
+            (new Example())
+                ->setId(1)
+                ->setName('Test name')
+                ->setCreatedAt(new DateTimeImmutable('2023-12-31T12:34:56Z'))
+        ];
+
+        yield [
+            '{"id":2,"name":"Second","createdAt":"2022-12-31T00:00:00Z"}',
+            (new Example())
+                ->setId(2)
+                ->setName('Second')
+                ->setCreatedAt(new DateTimeImmutable('2022-12-31T00:00:00Z'))
+        ];
     }
 }
