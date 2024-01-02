6ef84b1b608f1ae0ef2f7532494679ba6b3fab02
sakulb <95277083+sakulb@users.noreply.github.com>
2024-01-02 15:04:32 +0100

    Symfony 7 compatibility. (#18)
    * Removed SerializerParamConverter that depends on removed sensio/framework-extra-bundle. Added symfony 7.* compatibility. Changed testApp routes from removed annotations to attributes.

--- tests/config/routing/routing.php
+++ tests/config/routing/routing.php
@@ -5,5 +5,5 @@ declare(strict_types=1);
 namespace Symfony\Component\Routing\Loader\Configurator;
 
 return static function (RoutingConfigurator $routes): void {
-    $routes->import(resource: '../../TestApp/Controller', type: 'annotation');
+    $routes->import(resource: '../../TestApp/Controller', type: 'attribute');
 };
